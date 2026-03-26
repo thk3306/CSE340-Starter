26cd9535401fe11183cce5934bdd106aaf940cac
Emil Roding <emilroding27873@gmail.com>
2026-03-26 18:32:55 +1300

    Delivered Login View

---
+++ routes/accountRoute.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = new express.Router()
+const accountController = require("../controllers/accountController")
+const utilities = require("../utilities")
+
+router.get('/login', utilities.handleErrors(accountController.buildLogin))
+
+module.exports = router;
